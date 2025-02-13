4451eac4842642e179c108379bfe666bdb9b5110
niranjan_kumar <niranjanmani0011@gmail.com>
2025-02-13 20:15:56 +0530

    Add files via upload

---
+++ python_developer/task 3/bin.py
@@ -0,0 +1,2 @@
+n = int(input("Enter a decimal number: "))
+print(bin(n)[2:])  
---
+++ python_developer/task 3/check_sub.py
@@ -0,0 +1,3 @@
+s1 = input("Enter the main string: ")
+s2 = input("Enter the substring to search: ")
+print(s2 in s1)
